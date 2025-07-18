9727043122a112effaa4c7288e155ec175ab1e07
JayaramDhungana <[jayaramdhungana104@gmail.com]git config --global user.email [jayaramdhungana104@gmail.com]>
2025-07-18 09:17:13 +0545

    menu options was completed

--- lib/screens/over_view_screen.dart
+++ lib/screens/over_view_screen.dart
@@ -1,4 +1,5 @@
 import 'package:first_task/widgets/expandable_text_widget.dart';
+import 'package:first_task/widgets/over_view_options_widget.dart';
 import 'package:flutter/material.dart';
 
 class OverViewScreen extends StatefulWidget {
@@ -45,23 +46,66 @@ class _OverViewScreenState extends State<OverViewScreen> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(top: 20, left: 16),
-              child: Row(
-                children: [
-                  Text(
-                    "Pokhara",
-                    style: Theme.of(
-                      context,
-                    ).textTheme.bodyLarge!.copyWith(fontSize: 30),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20, left: 16),
-              child: ExpandableTextWidget(
-                text:
-                    "Pokhara is a beautiful city in Nepal known for its lakes, mountains, and natural beauty. It is a tourist hub and offers activities like paragliding, boating, trekking, and more. The city is also a gateway to the Annapurna mountain range.",
+              padding: const EdgeInsets.only(top: 20, left: 16, right: 16),
+              child: SizedBox(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "Pokhara",
+                          style: Theme.of(
+                            context,
+                          ).textTheme.bodyLarge!.copyWith(fontSize: 30),
+                        ),
+                      ],
+                    ),
+
+                    //for ExpandableText Widget
+                    ExpandableTextWidget(
+                      text:
+                          "Pokhara is a beautiful city in Nepal known for its lakes, mountains, and natural beauty. It is a tourist hub and offers activities like paragliding, boating, trekking, and more. The city is also a gateway to the Annapurna mountain range.",
+                    ),
+
+                    SizedBox(height: 16),
+
+                    //for Options
+                    Column(
+                      children: [
+                        overViewOptionWidget(
+                          context: context,
+                          text: "Hotels",
+                          icon: Icon(Icons.hotel, color: Colors.white),
+                        ),
+                        overViewOptionWidget(
+                          context: context,
+                          text: "Things to do",
+                          icon: Icon(
+                            Icons.camera_alt_outlined,
+                            color: Colors.white,
+                          ),
+                        ),
+                        overViewOptionWidget(
+                          context: context,
+                          text: "Restaurants",
+                          icon: Icon(Icons.restaurant, color: Colors.white),
+                        ),
+                        overViewOptionWidget(
+                          context: context,
+                          text: "Forums",
+                          icon: Icon(Icons.forum, color: Colors.white),
+                          isLast: true,
+                        ),
+
+                        //For Hotels
+
+                        
+                      ],
+                    ),
+
+                    SizedBox(height: 300),
+                  ],
+                ),
               ),
             ),
           ],
