583d69cd2869157930879c982fb7b7ba4f349625
JayaramDhungana <[jayaramdhungana104@gmail.com]git config --global user.email [jayaramdhungana104@gmail.com]>
2025-07-18 07:47:27 +0545

    expandable text feature implemented

--- lib/screens/over_view_screen.dart
+++ lib/screens/over_view_screen.dart
@@ -1,3 +1,4 @@
+import 'package:first_task/widgets/expandable_text_widget.dart';
 import 'package:flutter/material.dart';
 
 class OverViewScreen extends StatefulWidget {
@@ -19,30 +20,52 @@ class _OverViewScreenState extends State<OverViewScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          SizedBox(
-            height: 450,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: listOfOverViewImages.length,
-              itemBuilder: (context, index) {
-                final imageToShow = listOfOverViewImages[index];
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    height: 400,
-                    width: 300,
-                    child: Image(
-                      image: AssetImage(imageToShow),
-                      fit: BoxFit.fill,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 450,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: listOfOverViewImages.length,
+                itemBuilder: (context, index) {
+                  final imageToShow = listOfOverViewImages[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 400,
+                      width: 300,
+                      child: Image(
+                        image: AssetImage(imageToShow),
+                        fit: BoxFit.fill,
+                      ),
                     ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.only(top: 20, left: 16),
+              child: Row(
+                children: [
+                  Text(
+                    "Pokhara",
+                    style: Theme.of(
+                      context,
+                    ).textTheme.bodyLarge!.copyWith(fontSize: 30),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20, left: 16),
+              child: ExpandableTextWidget(
+                text:
+                    "Pokhara is a beautiful city in Nepal known for its lakes, mountains, and natural beauty. It is a tourist hub and offers activities like paragliding, boating, trekking, and more. The city is also a gateway to the Annapurna mountain range.",
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
